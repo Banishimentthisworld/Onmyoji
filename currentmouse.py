1fe563bc1c9a2dd6f839fb3e1e5fe70f9e2c659d
Banishimentthisworld <lj199605155231@gmail.com>
2019-02-22 02:24:55 +0800

    输出边框和鼠标信息

--- currentmouse.py
+++ currentmouse.py
@@ -1,5 +1,11 @@
 import pyautogui
+# import syd
+import win32gui, win32ui, win32con, win32api
 i = 1
 while i == 1:
+    titlename = "阴阳师-网易游戏"
+    hwnd = win32gui.FindWindow(0, titlename)
+    left, top, right, bottom = win32gui.GetWindowRect(hwnd)
+    print('左上x：' + str(left) + ' 左上y：' + str(top) + '  右下x：' + str(right) + ' 右下y：' + str(bottom))
     currentMouseX, currentMouseY = pyautogui.position()
     print('X轴=' + str(currentMouseX) + '  Y轴=' + str(currentMouseY))
