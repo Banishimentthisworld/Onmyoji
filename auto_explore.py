95027048e570049a5482e1f4f9ca1b2c3858ae21
Banishimentthisworld <lj199605155231@gmail.com>
2019-02-21 13:31:39 +0800

    自动探索测试

--- auto_explore.py
+++ auto_explore.py
@@ -4,7 +4,7 @@ import pyautogui
 import win32gui, win32ui, win32con, win32api
 import cv2
 import random
-
+import numpy as np
 
 def window_capture(filename):
     # 窗口的编号，0号表示当前活跃窗口
@@ -24,12 +24,12 @@ def window_capture(filename):
     # 获取左上和右下的坐标
     left, top, right, bottom = win32gui.GetWindowRect(hwnd)
     # 截图图片的宽
-    w = int((right - left) * 0.9)
+    w = int((right - left) * 0.8)
     # 截图图片的高
     h = int((bottom - top) * 0.4)
     # 截取图片的起点
-    s = left
-    e = int(top+(bottom-top)*0.3)
+    s = int(left + (right - left) * 0.1)
+    e = int(top + (bottom - top) * 0.3)
     # 为bitmap开辟空间
     saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
     # 高度saveDC，将截图保存到saveBitmap中
@@ -43,7 +43,7 @@ def cycle_capture(img_name):
     # 载入图片
     img = cv2.imread(img_name)
     # 缩放图片
-    img = cv2.resize(img, (873, 230))
+    img = cv2.resize(img, (776, 230))
     # 降噪（模糊处理用来减少瑕疵点）
     result = cv2.blur(img, (5, 5))
     # 灰度化,就是去色（类似老式照片）
@@ -51,7 +51,7 @@ def cycle_capture(img_name):
     # param1的具体实现，用于边缘检测
     canny = cv2.Canny(img, 40, 80)
     # 霍夫变换圆检测
-    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=35, minRadius=20, maxRadius=40)
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=35, minRadius=20, maxRadius=45)
     if circles is None:
         return None
     else:
@@ -66,57 +66,73 @@ def cycle_capture(img_name):
             img = cv2.circle(img, (x, y), r, (0, 0, 255), 1, 8, 0)
         # 显示新图像
         cv2.destroyAllWindows()
-        img = cv2.resize(img, (int(873/1.5), int(230/1.5)))
+        img = cv2.resize(img, (int(776/1.5), int(230/1.5)))
         cv2.namedWindow('ji', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('ji', img)
         cv2.waitKey(1)
-        return circles[0]
+        return circles[0][::-1]
 
 
-while True:
-    # 随机延时，抖动
-    tr = random.uniform(0.5, 1)
-    # 窗口置顶
-    titlename = "阴阳师-网易游戏"
-    hwnd = win32gui.FindWindow(0, titlename)
-    win32gui.EnableWindow(hwnd, True)
-    win32gui.SetForegroundWindow(hwnd)
-    # 获取窗口信息
-    left, top, right, bottom = win32gui.GetWindowRect(hwnd)
-    start_bottom = [(left + (right - left) * 0.876), (top + (bottom - top) * 0.723)]
-    # 截图
-    for i in range(10):
-        window_capture("ji.jpg")
-    # 查找圆
-    cycles = cycle_capture('ji.jpg')
+if __name__ == '__main__':
+    None_k = 0
+    while True:
+        # 随机延时，抖动
+        tr = random.uniform(0.5, 1)
+        # 窗口置顶
+        titlename = "阴阳师-网易游戏"
 
-    # 如果没有检测到圆，就点击开始按钮
-    if cycles is None:
-        print('少女祈祷中')
-        pr = random.uniform(3, 10)
-        pyautogui.moveTo(start_bottom[0] + pr, start_bottom[1] + pr)
-        pyautogui.click()
-        time.sleep(tr)
-    else:
-        for cycle in cycles:
-            pr = random.uniform(3, 10)
-            print('Master!圆形发现！ x：' + str(cycle[0]) + '  y：' + str(cycle[1]) + '  r：' + str(cycle[2]))
-            # 如果检测到圆，就点击圆心
-            x1 = int(left + ((cycle[0] / 970) * (right - left)))
-            y1 = int(top + (((cycle[1] + 172.5) / 575) * (bottom - top)))
-            pyautogui.moveTo(x1, y1)
+        hwnd = win32gui.FindWindow(0, titlename)
+        win32gui.EnableWindow(hwnd, True)
+        win32gui.SetForegroundWindow(hwnd)
+        # 获取窗口信息
+        left, top, right, bottom = win32gui.GetWindowRect(hwnd)
+        start_bottom = [(left + (right - left) * 0.876), (top + (bottom - top) * 0.723)]
+        explore_bottom = [(left + (right - left) * 0.734), (top + (bottom - top) * 0.759)]
+        left_bottom = [(left + (right - left) * 0.3), (top + (bottom - top) * 0.759)]
+        # 截图
+        for i in range(10):
+            window_capture("ji.jpg")
+        # 查找圆
+        cycles = cycle_capture('ji.jpg')
+
+        # 如果没有检测到圆，就点击开始按钮
+        if cycles is None:
+            if None_k > 10:
+                pyautogui.moveTo(left_bottom[0] + pr, left_bottom[1] + pr)
+                pyautogui.click()
+                time.sleep(tr)
+            else:
+                print('少女祈祷中')
+                pr = random.uniform(3, 10)
+                pyautogui.moveTo(start_bottom[0] + pr, start_bottom[1] + pr)
+                pyautogui.click()
+                time.sleep(tr)
+                # 超过十次没有发现圆，可能阴阳师走到了最右边
+                None_k = None_k + 1
+
+        else:
+            # 计数清零
+            None_k = 0
+            for cycle in cycles:
+                pr = random.uniform(3, 10)
+                print('Master!圆形发现！ x：' + str(cycle[0]) + '  y：' + str(cycle[1]) + '  r：' + str(cycle[2]))
+                # 如果检测到圆，就点击圆心
+                x1 = int(left + (((cycle[0] + 97) / 970) * (right - left)))
+                y1 = int(top + (((cycle[1] + 172.5) / 575) * (bottom - top)))
+                pyautogui.moveTo(x1, y1)
+                pyautogui.click()
+                time.sleep(tr)
+                # 排除结算界面的干扰，点击两下
+                pyautogui.moveTo(start_bottom[0] + pr, start_bottom[1] + pr)
+                pyautogui.click()
+                time.sleep(tr)
+                pyautogui.click()
+                time.sleep(tr)
+            # 再次进入副本
+            pyautogui.moveTo(explore_bottom[0] + pr, explore_bottom[1] + pr)
             pyautogui.click()
             time.sleep(tr)
-            # 排除结算界面的干扰，点击两下
-            pyautogui.moveTo(start_bottom[0] + pr, start_bottom[1] + pr)
-            pyautogui.click()
-            time.sleep(tr)
-            pyautogui.click()
-            time.sleep(tr)
 
-# 显示新图像
-# 按任意键退出1
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+
 
 
